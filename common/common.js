3876d611c93484f489ee87dd20e48846be7e1941
wanghao2 <wanghao2@gomeholdings.com>
2017-12-11 10:59:22 +0800

    在线留言

--- common/common.js
+++ common/common.js
@@ -2,3 +2,21 @@ $("#header").load("common/header.html");
 $("#footer").load("common/footer.html");
 $("#myTop").load("common/myTop.html");
 $("#myBotton").load("common/myBotton.html");
+
+
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate;
+    return currentdate;
+}
